316009da6d1fc5aebccc7db6e7820875b89ab8da
Brasdrive <86996894+DavidCamejo@users.noreply.github.com>
2025-07-28 08:58:45 -0400

    Update smtp-settings.php

--- storage/snippets/smtp-settings.php
+++ storage/snippets/smtp-settings.php
@@ -2,15 +2,16 @@
 // Configure SMTP settings
 
 if (!defined('ABSPATH')) exit;
+
 function setup_phpmailer_init( $phpmailer ) {
     $phpmailer->IsSMTP();
-    $phpmailer->Host = 'smtp-relay.sendinblue.com';
+    $phpmailer->Host = 'smtp.smtpexample.com';
     $phpmailer->Port = 587;
     $phpmailer->SMTPSecure = 'tls';
     $phpmailer->SMTPAuth = true;
-    $phpmailer->Username = 'jdavidcamejo@gmail.com';
-    $phpmailer->Password = 'Ij8DcFwLxmZM6ytX';
-    $phpmailer->From = 'web@brasdrive.com.br';
-    $phpmailer->FromName = 'Brasdrive';
+    $phpmailer->Username = 'yourname@yoursite.com';
+    $phpmailer->Password = 'XXXXXXXXXXXXXXXX';
+    $phpmailer->From = 'yourweb@yoursite.com';
+    $phpmailer->FromName = 'Your Web';
 }
 add_action( 'phpmailer_init', 'setup_phpmailer_init' );
